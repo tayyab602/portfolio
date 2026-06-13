ece5ff61bd0819d4daa64788ddd056e436b458b1
TAYYAB NAVEEED AKHTAR <tayyabnaveed13@gmail.com>
2026-06-14 01:23:06 +0500

    Update vite.config.ts

--- artifacts/portfolio/vite.config.ts
+++ artifacts/portfolio/vite.config.ts
@@ -14,7 +14,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
     runtimeErrorOverlay(),
-
+    
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
             m.cartographer({

524d1b7588e7a800d66ada455190d0df8ffbafb2
TAYYAB NAVEEED AKHTAR <tayyabnaveed13@gmail.com>
2026-06-14 01:02:26 +0500

    Update tooltip.tsx

--- artifacts/portfolio/src/components/ui/tooltip.tsx
+++ artifacts/portfolio/src/components/ui/tooltip.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import * as Tooltip from "@radix-ui/react-tooltip";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
 import { cn } from "@/lib/utils"

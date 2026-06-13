70ec906f16c49cb3399cb5eb9eea432b0acd41ab
TAYYAB NAVEEED AKHTAR <tayyabnaveed13@gmail.com>
2026-06-14 01:04:07 +0500

    Update tooltip.tsx

--- artifacts/portfolio/src/components/ui/tooltip.tsx
+++ artifacts/portfolio/src/components/ui/tooltip.tsx
@@ -1,16 +1,13 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as Tooltip from "@radix-ui/react-tooltip";
-import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import * as React from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const TooltipProvider = TooltipPrimitive.Provider
-
-const Tooltip = TooltipPrimitive.Root
-
-const TooltipTrigger = TooltipPrimitive.Trigger
+const TooltipProvider = TooltipPrimitive.Provider;
+const Tooltip = TooltipPrimitive.Root;
+const TooltipTrigger = TooltipPrimitive.Trigger;
 
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
@@ -27,7 +24,8 @@ const TooltipContent = React.forwardRef<
       {...props}
     />
   </TooltipPrimitive.Portal>
-))
-TooltipContent.displayName = TooltipPrimitive.Content.displayName
+));
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };

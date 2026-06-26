301bbb6d91649e506c05b6040c4e6c20ca8fdc5c
TAYYAB NAVEEED AKHTAR <tayyabnaveed13@gmail.com>
2026-06-26 15:01:31 +0500

    Wrap App component with RobotProvider

--- artifacts/portfolio/src/main.tsx
+++ artifacts/portfolio/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { RobotProvider } from "./robot/RobotContext";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <RobotProvider>
+    <App />
+  </RobotProvider>
+);
